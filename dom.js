ab277fe9271c2fad1085172bed0eb6aa9da5ea48
rb50 <rebecca-holmes@live.co.uk>
2017-11-17 13:15:41 +0000

    api working

--- dom.js
+++ dom.js
@@ -20,6 +20,45 @@ inputfield.addEventListener('keydown', function(e){
   xhttp.send(inputtext);
 }, false);
 
+
+var submitform= document.getElementById('the-form');
+submitform.addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log(e);
+  var actress = e.srcElement[0].value;
+  console.log(e.srcElement[0].value);
+  var characters = {
+    "Buffy Summers": "sarah+michelle+gellar",
+    "Angel" : "david+boreanaz",
+    "Willow Rosenberg": "alyson+hannigan",
+    "Spike": "james+marsters",
+    "Dawn Summers": "michelle+trachtenberg",
+    "Rupert Giles": "anthony+head",
+    "Cordelia": "eliza+dushku"
+  }
+  var actress = characters[e.srcElement[0].value];
+  console.log(e.srcElement[0].value);
+  // if(actress){
+
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var obj = JSON.parse(xhr.responseText);
+      console.log(obj);
+      var info = obj["name_popular"][0]["description"];
+      console.log(info);
+      updateInfo(info);
+
+    }
+  };
+  xhr.open("GET", 'https://cors-anywhere.herokuapp.com/http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q='+actress , true);
+  xhr.send();
+}, false);
+// }
+
+
+
 function updateDom(name){
   var dataList = document.getElementById('dropdown');
   while(dataList.firstChild){
@@ -30,4 +69,19 @@ function updateDom(name){
     option.value = name[i];
     dataList.appendChild(option);
   }
+}
+
+function updateInfo(info) {
+  console.log(info);
+  var divElement = document.getElementById('info');
+  while(divElement.firstChild){
+    divElement.removeChild(divElement.firstChild);
+  }
+  var para = document.createElement('p');
+  var infotext = document.createTextNode(info);
+  para.appendChild(infotext);
+  divElement.appendChild(para);
+  divElement.className('info');
+
+
 }
